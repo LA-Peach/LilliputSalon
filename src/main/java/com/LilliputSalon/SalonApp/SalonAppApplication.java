f701cdf8947bd034f9dd069f3cb8e6f7e8f90b78
LA-Peach <leaannwillis@student.purdueglobal.edu>
2025-11-24 20:19:47 -0500

    Spring Initilizr

---
+++ src/main/java/com/LilliputSalon/SalonApp/SalonAppApplication.java
@@ -0,0 +1,13 @@
+package com.LilliputSalon.SalonApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalonAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalonAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LilliputSalon/SalonApp/SalonAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LilliputSalon.SalonApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalonAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
